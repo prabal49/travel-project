6c429bfc296324ecebfb932ff4830d722b8ce881
prabal49 <35prabal@gmail.com>
2026-07-28 23:19:00 +0530

    neww login error

--- frontend/travel-with-vibes/src/pages/Login.jsx
+++ frontend/travel-with-vibes/src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const API_URL = `import.meta.env.VITE_API_URL`;
+  const API_URL = import.meta.env.VITE_API_URL;
 
   // ================= REGISTER =================
 
